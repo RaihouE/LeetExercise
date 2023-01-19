b19859ba4be90eba3b74c5f6750087ba4831052a
Filip Gmyzo <filip.gmyzo@gmail.com>
2023-01-19 17:33:55 +0100

    Add project files.

---
+++ Leet.Solutions/Solutions.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Linq;
+using System;
+
+namespace Leet.Solutions;
+
+public class Solutions
+{
+    /// <summary>
+    /// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). 
+    /// Return the running sum of nums.
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public static int[] RunningSum(int[] nums)
+    {
+        var result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            //result[i] = nums[0..(i + 1)].Sum(); //this is simpler IMO
+
+            var sum = 0;
+            for (var j = 0; j < i + 1; j++)
+            {
+                sum += nums[j];
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Given an array of integers nums, calculate the pivot index of this array.
+    ///The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+    ///If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left.This also applies to the right edge of the array.
+    ///Return the leftmost pivot index.If no such index exists, return -1.
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public static int PivotIndex(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var leftSum = nums[0..i].Sum();
+            var rightSum = nums[(i + 1)..nums.Length].Sum();
+
+            if (leftSum == rightSum)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Leet.Tests/SolutionsTests.cs
@@ -0,0 +1,28 @@
+using Leet.Solutions;
+
+namespace Leet.Tests;
+
+public class SolutionsTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 4 }, new[] { 1, 3, 6, 10 })]
+    [InlineData(new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new[] { 3, 1, 2, 10, 1 }, new[] { 3, 4, 6, 16, 17 })]
+    public void IsRunningSum(int[] input, int[] output)
+    {
+        var runningSum = Solutions.Solutions.RunningSum(input);
+
+        Assert.Equal(output, runningSum);
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 7, 3, 6, 5, 6 }, 3)]
+    [InlineData(new[] { 1, 2, 3 }, -1)]
+    [InlineData(new[] { 2, 1, -1 }, 0)]
+    public void IsPivotIndex(int[] input, int output)
+    {
+        var pivotIndex = Solutions.Solutions.PivotIndex(input);
+
+        Assert.Equal(output, pivotIndex);
+    }
+}
